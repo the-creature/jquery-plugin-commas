3f121877d1d0f39cdcf55ab400339ac7dbdc61a4
the-creature <latin.creature@gmail.com>
2014-03-13 18:04:23 -0400

    add comma script

---
+++ script.js
@@ -0,0 +1,40 @@
+window.TEMPLATE = (function($) {
+	var SELF;
+
+	return {
+		init: function() {
+        $(document.body).bind("render", function() {
+            window.TEMPLATE.addCommas();
+		});
+		},
+
+		addCommas : function() {
+		    function commaSeparateNumber(val) {
+			    while (/(\d+)(\d{3})/.test(val.toString())){
+				    val = val.toString().replace(/(\d+)(\d{3})/, '$1'+','+'$2');
+				}
+				
+                return val;
+			}
+
+			$(".j-num").each(function() {
+				var $this = $(this),
+					num = parseInt($this.html());
+				
+				$this.html(commaSeparateNumber(num)).removeClass('j-num');
+			});
+		}
+	}
+}(jQuery));
+
+$(document).ready(function() {
+	window.TEMPLATE.init();
+    
+    $.fn.render = function(template) {
+		var elm = this;
+		elm.html(template);
+		elm.trigger("render");
+	};
+
+    $('.j-add-comma').render( $('.j-add-comma').data('number') );
+});
