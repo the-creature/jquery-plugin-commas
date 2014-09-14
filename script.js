a519d22e15fceba2177f3c02b10d1a403ec49b99
Liccy <the-creature@users.noreply.github.com>
2014-09-13 22:18:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
 window.TEMPLATE = (function($) {
-	var SELF;
+    var SELF;
 
-	return {
-	    init: function() {
-			$(document.body).bind("render", function() {
-					window.TEMPLATE.addCommas();
-		});
-	    },
+    return {
+        init: function() {
+            $(document.body).bind("render", function() {
+                window.TEMPLATE.addCommas();
+            });
+        },
 
-	    addCommas : function() {
-		function commaSeparateNumber(val) {
-			while (/(\d+)(\d{3})/.test(val.toString())) {
-				val = val.toString().replace(/(\d+)(\d{3})/, '$1'+','+'$2');
-		}
-			
-					return val;
-		}
+        addCommas: function() {
+            function commaSeparateNumber(val) {
+                while (/(\d+)(\d{3})/.test(val.toString())) {
+                    val = val.toString().replace(/(\d+)(\d{3})/, '$1' + ',' + '$2');
+                }
 
-		$(".j-num").each(function() {
-			var $this = $(this),
-			num = parseInt($this.html());
-			
-		$this.html(commaSeparateNumber(num)).removeClass('j-num');
-		});
-	    }
-	}
+                return val;
+            }
+
+            $(".j-num").each(function() {
+                var $this = $(this),
+                    num = parseInt($this.html());
+
+                $this.html(commaSeparateNumber(num)).removeClass('j-num');
+            });
+        }
+    }
 }(jQuery));
 
 $(document).ready(function() {
     window.TEMPLATE.init();
-    
-    $.fn.render = function(template) {
-		      var elm = this;
-		      
-		      elm.html(template);
-		      elm.trigger("render");
-	          };
 
-    $('.j-add-comma').render( $('.j-add-comma').data('number') );
+    $.fn.render = function(template) {
+        var elm = this;
+
+        elm.html(template);
+        elm.trigger("render");
+    };
+
+    $('.j-add-comma').render($('.j-add-comma').data('number'));
 });
